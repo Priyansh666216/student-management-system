6bb3b9c4b4cbb1ecf38acaf1e1fffe7f5371807d
Priyanshu shukla <priyanshushukla73700@gmail.com>
2026-04-25 22:29:55 +0530

    Add Dockerfile, .env config, update API URL for Railway deployment

--- src/services/studentService.js
+++ src/services/studentService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api/students";
+const BASE_URL = `${process.env.REACT_APP_API_URL || "http://localhost:8080"}/api/students`;
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
